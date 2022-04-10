ca1630e2e14f7859bee10cf13ace736a52e9a706
BeautyYuYanli <32453863+BeautyYuYanli@users.noreply.github.com>
2022-04-10 16:56:54 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,16 +1,16 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
-  link: 'https://myblog-zh.vercel.app',
-  description: 'This gonna be an awesome website.',
+  title: '银河美术馆',
+  author: 'Beautyyu言醴',
+  email: 'beautyyuyanli@gmail.com',
+  link: 'https://blog-zh.beautyyu.one/',
+  description: '我们常常高估一天能做成的事，却往往低估一年能做的事。',
   lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
-  appearance: 'auto', // ['light', 'dark', 'auto'],
+  appearance: 'light', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
   lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
   darkBackground: '#18181B', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
-  since: 2021, // If leave this empty, current year will be used.
+  since: 2022, // If leave this empty, current year will be used.
   postsPerPage: 7,
   sortByDate: false,
   showAbout: true,
@@ -18,9 +18,9 @@ const BLOG = {
   showLink: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
-  socialLink: 'https://twitter.com/craigaryhart',
+  socialLink: '',
   seo: {
-    keywords: ['Blog', 'Website', 'Notion'],
+    keywords: ['博客', '网页', 'Notion'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
   },
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
